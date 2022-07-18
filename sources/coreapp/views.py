ad9e3d6fd240966eda7c85deda574463d09f37ad
Iaroslav Os <osmnbx@gmail.com>
2022-07-18 17:43:28 +0300

    first views

--- sources/coreapp/views.py
+++ sources/coreapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def coreapp(request):
+    return HttpResponse('Страница coreapp')
+
+def categories(request):
+    return HttpResponse('Страница categories')
