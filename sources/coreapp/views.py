fb3cdcf4c8a1234e6759fbabad68412a08f8c7e6
Iaroslav Os <osmnbx@gmail.com>
2022-07-19 15:54:39 +0300

    views updated +new

--- sources/coreapp/views.py
+++ sources/coreapp/views.py
@@ -1,12 +1,20 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 def index(request):
     return HttpResponse('Страница index')
 
 def categories(request, id):
+    if request.GET:
+        print(request.GET)
     return HttpResponse(f'Страница categories, {id}')
 
 def archive(request, year):
+    if int(year) > 2022:
+        raise Http404()
     return HttpResponse(f'Страница archive, {year}')
+
+def page_404(request, exception):
+    return HttpResponseNotFound('Страница не найдена!')
+    
