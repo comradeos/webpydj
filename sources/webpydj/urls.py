8801b54f032b152b0e165a0a756e65a63ba25c07
Iaroslav Os <osmnbx@gmail.com>
2022-07-19 10:30:30 +0300

    urls updated

--- sources/webpydj/urls.py
+++ sources/webpydj/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('coreapp/', include('coreapp.urls')),
+    path('', include('coreapp.urls')),
 ]
