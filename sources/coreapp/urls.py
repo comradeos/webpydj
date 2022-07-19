dfd3d3a9e4da633645995a6b0c83c2ed2be46e65
Iaroslav Os <osmnbx@gmail.com>
2022-07-19 20:02:48 +0300

    updated urls

--- sources/coreapp/urls.py
+++ sources/coreapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, re_path
 from coreapp.views import *
 
 urlpatterns = [
-    path('', index),
+    path('home/', index, name='home'),
     path('categories/<str:id>/', categories), # <str: int: slug: uuid: path:>
     
     re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
