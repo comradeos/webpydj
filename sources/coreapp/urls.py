7234a12176debdb84413d05cb9606a4257e0d5c5
Iaroslav Os <osmnbx@gmail.com>
2022-07-24 11:54:02 +0300

    added 3 more urls

--- sources/coreapp/urls.py
+++ sources/coreapp/urls.py
@@ -5,10 +5,8 @@ from coreapp.views import *
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
-    
-    path('categories/<str:id>/', categories), # <str: int: slug: uuid: path:>
-    
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
-    
+    path('add-page/', add_page, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
 ]
 
