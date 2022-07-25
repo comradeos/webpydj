1be87b4bc571a4a43653cd1e5354e1204ef39f69
Iaroslav Os <osmnbx@gmail.com>
2022-07-25 13:46:54 +0300

    upd index method: + cats, added show_category method

--- sources/coreapp/views.py
+++ sources/coreapp/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404, HttpResponse, HttpResponseNotFound
 from django.shortcuts import redirect, render
-from coreapp.models import Languages
+from coreapp.models import Categories, Languages
 
 
 menu = [
@@ -17,9 +17,12 @@ def page_404(request, exception):
 
 def index(request):
     posts = Languages.objects.all()
+    cats = Categories.objects.all()
     context = {
         'menu': menu,
         'posts': posts,
+        'cats': cats,
+        'cat_selected': 0,
         'title': 'Index Page',
     }
     return render(request, 'coreapp/index.html', context=context)
@@ -30,3 +33,4 @@ def add_page(request): return HttpResponse("add_page page")
 def contact(request): return HttpResponse("contact page")
 def login(request): return HttpResponse("login page")
 def show_post(request, post_id): return HttpResponse(f"show_post id {post_id}")
+def show_category(request, category_id): return HttpResponse(f"show category id {category_id}")
