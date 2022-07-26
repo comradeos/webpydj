aee067d1fb1da840fa70dd878d68c5e68b77baf1
Iaroslav Os <osmnbx@gmail.com>
2022-07-26 19:28:19 +0300

    admin panel settings updated

--- sources/coreapp/admin.py
+++ sources/coreapp/admin.py
@@ -7,6 +7,7 @@ class LanguagesAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
     list_display_links = ('id', 'title')
     search_field = ('title', 'content')
+    list_filter = ('is_published', 'time_create')
 
 admin.site.register(Languages, LanguagesAdmin)
 
