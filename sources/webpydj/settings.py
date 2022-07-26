e86a15d0d860af84fa92b98d56af630e7995336e
Iaroslav Os <osmnbx@gmail.com>
2022-07-26 16:49:09 +0300

    language changed for 127.0.0.1:8001/admin

--- sources/webpydj/settings.py
+++ sources/webpydj/settings.py
@@ -114,8 +114,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-# LANGUAGE_CODE = 'ru'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 # LANGUAGE_CODE = 'uk'
 
 TIME_ZONE = 'UTC'
