0bd2a4e6603ae16608d06ec4b5cff9879e055959
Iaroslav Os <osmnbx@gmail.com>
2022-07-26 17:28:43 +0300

    added LanguagesAdmin class

--- sources/coreapp/admin.py
+++ sources/coreapp/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from coreapp.models import Languages, Categories
 
 # Register your models here.
-admin.site.register(Languages)
+
+class LanguagesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_field = ('title', 'content')
+
+admin.site.register(Languages, LanguagesAdmin)
+
 admin.site.register(Categories)
