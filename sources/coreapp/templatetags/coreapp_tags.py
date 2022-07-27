f88f0a4de019731d0ddf27a7703df0716cd302d7
Iaroslav Os <osmnbx@gmail.com>
2022-07-27 11:38:01 +0300

    upd: added new inctag 'menu'

--- sources/coreapp/templatetags/coreapp_tags.py
+++ sources/coreapp/templatetags/coreapp_tags.py
@@ -3,14 +3,33 @@ from coreapp.models import *
 
 register = template.Library()
 
-@register.simple_tag()
+
+@register.simple_tag(name='getcats')
 def get_categories(filter=None):
     if not filter:
         return Categories.objects.all()
     else:
         return Categories.objects.filter(pk=filter)
-    
+
+
 @register.inclusion_tag('coreapp/list_categories.html')
-def show_categories():
-    cats = Categories.objects.all()
-    return {'cats': cats}
+def show_categories(sort=None, cat_selected=0):
+    if not sort:
+        cats = Categories.objects.all()
+    else:
+        cats = Categories.objects.order_by(sort)
+    return {
+        'cats': cats,
+        'cat_selected': cat_selected,
+    }
+
+
+@register.inclusion_tag('coreapp/menu.html')
+def show_menu():
+    menu = [
+        {'title': 'About', 'url_name': 'about', },
+        {'title': 'Add Page', 'url_name': 'add_page', },
+        {'title': 'Contact', 'url_name': 'contact', },
+        {'title': 'Login', 'url_name': 'login', },
+    ]
+    return { 'menu': menu, }
