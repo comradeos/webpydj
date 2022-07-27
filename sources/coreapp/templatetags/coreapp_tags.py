ca1ed2a51b905cb8b6f09f096c32b7dbf2cfcc18
Iaroslav Os <osmnbx@gmail.com>
2022-07-27 11:12:45 +0300

    updated coreapp_tags

--- sources/coreapp/templatetags/coreapp_tags.py
+++ sources/coreapp/templatetags/coreapp_tags.py
@@ -4,5 +4,13 @@ from coreapp.models import *
 register = template.Library()
 
 @register.simple_tag()
-def get_categories():
-    return Categories.objects.all()
+def get_categories(filter=None):
+    if not filter:
+        return Categories.objects.all()
+    else:
+        return Categories.objects.filter(pk=filter)
+    
+@register.inclusion_tag('coreapp/list_categories.html')
+def show_categories():
+    cats = Categories.objects.all()
+    return {'cats': cats}
