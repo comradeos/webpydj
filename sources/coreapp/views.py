42dfe436fe86bb49329239678ca61c1519dba9d1
Iaroslav Os <osmnbx@gmail.com>
2022-07-28 09:24:02 +0300

    + show_post function (view)

--- sources/coreapp/views.py
+++ sources/coreapp/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404, HttpResponse, HttpResponseNotFound
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from coreapp.models import Categories, Languages
 
 
@@ -17,13 +17,6 @@ def index(request):
     return render(request, 'coreapp/index.html', context=context)
 
 
-def about(request): return HttpResponse("about page")
-def add_page(request): return HttpResponse("add_page page")
-def contact(request): return HttpResponse("contact page")
-def login(request): return HttpResponse("login page")
-def show_post(request, post_id): return HttpResponse(f"show_post id {post_id}")
-
-
 def show_category(request, cat_id): 
     posts = Languages.objects.filter(cat_id=cat_id)
     
@@ -36,3 +29,19 @@ def show_category(request, cat_id):
         'title': 'Category Page',
     }
     return render(request, 'coreapp/index.html', context=context)
+
+
+def show_post(request, post_id): 
+    post = get_object_or_404(Languages, pk=post_id)
+    context = {
+        'post': post,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+    return render(request, 'coreapp/post.html', context=context)
+
+
+def about(request): return HttpResponse("about page")
+def add_page(request): return HttpResponse("add_page page")
+def contact(request): return HttpResponse("contact page")
+def login(request): return HttpResponse("login page")
