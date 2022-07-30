bed1d97b9eec63d499a99cbda53c9613d5da1e23
Iaroslav Os <osmnbx@gmail.com>
2022-07-30 12:46:52 +0300

    created custom labels

--- sources/coreapp/forms.py
+++ sources/coreapp/forms.py
@@ -5,12 +5,13 @@ from coreapp.views import Categories, Languages
 class AddPostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['photo'].label = 'Logo'
+        self.fields['cat'].label = 'Category'
         self.fields['cat'].empty_label = 'Please select'
     class Meta:
         model = Languages
-        fields = ['title', 'slug', 'content', 'is_published', 'cat']
+        fields = ['title', 'slug', 'content', 'is_published', 'cat', 'photo']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-input'}),
-            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10,}),
             'content': forms.Textarea(attrs={'cols': 60, 'rows': 10,}),
         }
