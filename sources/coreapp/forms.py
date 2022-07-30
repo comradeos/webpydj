caa64ef56172719c5c4373a27236e89243db8ed3
Iaroslav Os <osmnbx@gmail.com>
2022-07-30 11:33:37 +0300

    updated new form related to model

--- sources/coreapp/forms.py
+++ sources/coreapp/forms.py
@@ -1,10 +1,16 @@
 from django import forms
-from coreapp.views import Categories
+from coreapp.views import Categories, Languages
 
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Title')
-    slug = forms.SlugField(max_length=255, label='URL')
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label='Content')
-    is_published = forms.BooleanField(label='Published', required=False, initial=True)
-    cat = forms.ModelChoiceField(queryset=Categories.objects.all(), label='Category', empty_label='Please select')
+class AddPostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cat'].empty_label = 'Please select'
+    class Meta:
+        model = Languages
+        fields = ['title', 'slug', 'content', 'is_published', 'cat']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10,}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10,}),
+        }
