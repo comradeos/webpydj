18356bc858835e7165dc72d0ff8e7e50eaa7f728
Iaroslav Os <osmnbx@gmail.com>
2022-07-31 11:32:19 +0300

    updated url patterns

--- sources/coreapp/urls.py
+++ sources/coreapp/urls.py
@@ -3,12 +3,14 @@ from django.urls import path, re_path
 from coreapp.views import *
 
 urlpatterns = [
-    path('', index, name='index'),
+    # path('', index, name='index'),
+    path('', LanguagesHome.as_view(), name='index'),
     path('about/', about, name='about'),
     path('add-page/', add_page, name='add_page'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
     path('post/<slug:post_slug>/', show_post, name='post'),
-    path('category/<int:cat_id>/', show_category, name='category'),
+    # path('category/<int:cat_id>/', show_category, name='category'),
+    path('category/<slug:cat_slug>/', LanguagesCategory.as_view(), name='category'),
 ]
 
