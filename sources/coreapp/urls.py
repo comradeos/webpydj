465840401aad8dc8827e3fc6f8e8a584977445f5
Iaroslav Os <osmnbx@gmail.com>
2022-07-31 12:01:44 +0300

    urls patterns updated

--- sources/coreapp/urls.py
+++ sources/coreapp/urls.py
@@ -6,10 +6,16 @@ urlpatterns = [
     # path('', index, name='index'),
     path('', LanguagesHome.as_view(), name='index'),
     path('about/', about, name='about'),
-    path('add-page/', add_page, name='add_page'),
+    
+    # path('add-page/', add_page, name='add_page'),
+    path('add-page/', AddPage.as_view(), name='add_page'),
+    
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
-    path('post/<slug:post_slug>/', show_post, name='post'),
+    
+    # path('post/<slug:post_slug>/', show_post, name='post'),
+    path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
+    
     # path('category/<int:cat_id>/', show_category, name='category'),
     path('category/<slug:cat_slug>/', LanguagesCategory.as_view(), name='category'),
 ]
