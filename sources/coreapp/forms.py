3f0993d8698b8998101a944a2267464b3cc6ba12
Iaroslav Os <osmnbx@gmail.com>
2022-08-07 10:58:09 +0300

    updated

--- sources/coreapp/forms.py
+++ sources/coreapp/forms.py
@@ -25,17 +25,16 @@ class AddPostForm(forms.ModelForm):
     
 
 from django.contrib.auth.models import User
-class RegisterUserForm(forms.ModelForm):
+from django.contrib.auth.forms import UserCreationForm
+
+class RegisterUserForm(UserCreationForm):
     username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label='Password', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label='Repeat Password', widget=forms.TextInput(attrs={'class': 'form-input'}))
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2')
-        widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-input'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-input'}),
-        }
+        fields = ['username', 'email', 'password1', 'password2']
+
 
     
